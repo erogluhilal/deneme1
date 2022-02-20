efcc481dcc6251d9980e43a8c7c6178e394fd9cc
erogluhilal <erogluhilal00@gmail.com>
2022-02-20 18:59:38 +0300

    ilk commit

---
+++ deneme__1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main ()
+{ int plaka_kodu;
+printf("ilin plaka kodunu giriniz: ");
+scanf("%d",& plaka_kodu);
+switch(plaka_kodu)
+{
+    case 69;
+    printf("BAYBURT\n");
+    break;
+    case 34 ;
+    printf("İSTANBUL\n");
+    break;
+    case 6 ;
+    printf("ANKARA\n");
+    break;
+    case 37;
+    printf("KASTAMONU\n");
+    break;
+    case 35 ;
+    printf("İZMİR\n");
+    break;
+    case 55;
+    printf("SAMSUN\n");
+    break;
+    default;
+    printf("tanımsız plaka kodu\n");
+}
+return 0;
+
+}
